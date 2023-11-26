1d469768d05b399c705b25e3a7b79f9edf6adc52
Ron Galicia <dermalin3k@hotmail.com>
2023-11-26 00:49:57 -0800

    Update omaha_driller.py
    Allowing non-pairs.

--- omaha_driller.py
+++ omaha_driller.py
@@ -18,24 +18,33 @@ while True:
 
     user_input = input("Enter:")
 
-    suit_indices = list(range(4))
-    random.shuffle(suit_indices)
+    if paired == True:
+        suit_indices = list(range(4))
+        random.shuffle(suit_indices)
 
-    suit_index_1 = suit_indices.pop(0)
-    suit_1 = suits[suit_index_1]
-    rank_1 = random.choice(ranks)
+        suit_index_1 = suit_indices.pop(0)
+        suit_1 = suits[suit_index_1]
+        rank_1 = random.choice(ranks)
 
-    suit_index_2 = suit_indices.pop(0)
-    suit_2 = suits[suit_index_2]
+        suit_index_2 = suit_indices.pop(0)
+        suit_2 = suits[suit_index_2]
 
-    ## make pair
-    pocket.append(str(rank_1) + suit_1)
-    pocket.append(str(rank_1) + suit_2)
-    deck.remove(pocket[0])
-    deck.remove(pocket[1])
-    random.shuffle(deck)
-    pocket.append(deck.pop(0))
-    pocket.append(deck.pop(0))
+        ## make pair
+        pocket.append(str(rank_1) + suit_1)
+        pocket.append(str(rank_1) + suit_2)
+        deck.remove(pocket[0])
+        deck.remove(pocket[1])
+        random.shuffle(deck)
+        pocket.append(deck.pop(0))
+        pocket.append(deck.pop(0))
+    else:
+        random.shuffle(deck)
+        pocket.append(deck.pop(0))
+        pocket.append(deck.pop(0))
+        pocket.append(deck.pop(0))
+        pocket.append(deck.pop(0))
 
     random.shuffle(pocket)
     print(' '.join(pocket))
+
+

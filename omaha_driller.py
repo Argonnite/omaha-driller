7e8a4308aaab26a797acac5f25d914a803e01ef3
Ron Galicia <dermalin3k@hotmail.com>
2024-05-26 02:02:39 -0700

    Update omaha_driller.py
    NLHE added

--- omaha_driller.py
+++ omaha_driller.py
@@ -5,7 +5,8 @@ suit_grp = ['ds', 'ss', 'rb']
 suits = 'shdc'
 suits = list(suits)
 ranks = ['a', 2, 3, 4, 5, 6, 7, 8, 9, 't', 'j', 'q', 'k']
-
+position = ['LJ', 'HJ', 'CO', 'BTN', 'SB', 'BB']
+user_input = None
 
 while True:
 
@@ -16,36 +17,51 @@ while True:
             deck.append(str(rank) + suit)
     pocket = []
 
-    user_input = input("Enter:")
-
-    if paired == True:
-        suit_indices = list(range(4))
-        random.shuffle(suit_indices)
-
-        suit_index_1 = suit_indices.pop(0)
-        suit_1 = suits[suit_index_1]
-        rank_1 = random.choice(ranks)
-
-        suit_index_2 = suit_indices.pop(0)
-        suit_2 = suits[suit_index_2]
-
-        ## make pair
-        pocket.append(str(rank_1) + suit_1)
-        pocket.append(str(rank_1) + suit_2)
-        deck.remove(pocket[0])
-        deck.remove(pocket[1])
-        random.shuffle(deck)
-        pocket.append(deck.pop(0))
-        pocket.append(deck.pop(0))
-    elif paired == False:
-        random.shuffle(deck)
-        pocket.append(deck.pop(0))
-        pocket.append(deck.pop(0))
-        pocket.append(deck.pop(0))
-        pocket.append(deck.pop(0))
+    if user_input is None:
+        user_input = input("Enter (1=NLHE, 2=PLO):")
     else:
-        pocket = deck.copy()
+        prev_user_input = user_input
+        user_input = input("Enter (1=NLHE, 2=PLO):")
+        if user_input == "":
+            user_input = prev_user_input
 
-    random.shuffle(pocket)
-    print(' '.join(pocket))
+    if int(user_input) == 1:  ## NLHE
+        random.shuffle(position)
+        print(position.pop(0))
+        random.shuffle(deck)
+        pocket.append(deck.pop(0))
+        pocket.append(deck.pop(0))
+        random.shuffle(pocket)
+        print(' '.join(pocket))
+    elif int(user_input) == 2:  ## PLO
+        if paired == True:
+            suit_indices = list(range(4))
+            random.shuffle(suit_indices)
+
+            suit_index_1 = suit_indices.pop(0)
+            suit_1 = suits[suit_index_1]
+            rank_1 = random.choice(ranks)
+
+            suit_index_2 = suit_indices.pop(0)
+            suit_2 = suits[suit_index_2]
+
+            ## make pair
+            pocket.append(str(rank_1) + suit_1)
+            pocket.append(str(rank_1) + suit_2)
+            deck.remove(pocket[0])
+            deck.remove(pocket[1])
+            random.shuffle(deck)
+            pocket.append(deck.pop(0))
+            pocket.append(deck.pop(0))
+        elif paired == False:
+            random.shuffle(deck)
+            pocket.append(deck.pop(0))
+            pocket.append(deck.pop(0))
+            pocket.append(deck.pop(0))
+            pocket.append(deck.pop(0))
+        else:
+            pocket = deck.copy()
+
+        random.shuffle(pocket)
+        print(' '.join(pocket))
 
